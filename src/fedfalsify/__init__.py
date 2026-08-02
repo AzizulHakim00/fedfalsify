6b4b94acf0339df075c416cf31a474e3f7c4072a
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-08-02 19:38:21 +0600

    feat: expose version 0.2 benchmark API

--- src/fedfalsify/__init__.py
+++ src/fedfalsify/__init__.py
@@ -1,18 +1,34 @@
 """FedFalsify: counterexample-guided federated mechanism discovery."""
 
 from .basis import CandidateEquation, TermCatalog
+from .benchmarks import (
+    BenchmarkResult,
+    run_exception_benchmark,
+    run_spurious_correlation_benchmark,
+)
 from .client import FederatedFalsifierClient
-from .data import ClientDataset, generate_heterogeneous_clients
-from .server import DiscoveryResult, FedFalsifyDiscovery
+from .data import (
+    ClientDataset,
+    generate_exception_clients,
+    generate_heterogeneous_clients,
+    generate_spurious_correlation_clients,
+)
+from .server import DiscoveryResult, FedFalsifyDiscovery, RepairDecision
 
 __all__ = [
+    "BenchmarkResult",
     "CandidateEquation",
     "ClientDataset",
     "DiscoveryResult",
     "FedFalsifyDiscovery",
     "FederatedFalsifierClient",
+    "RepairDecision",
     "TermCatalog",
+    "generate_exception_clients",
     "generate_heterogeneous_clients",
+    "generate_spurious_correlation_clients",
+    "run_exception_benchmark",
+    "run_spurious_correlation_benchmark",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
